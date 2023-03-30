3207802b6c53becc420fd81d42c7f1caa6cd22a4
mohsin raza abid <125199140+mohsin-abi@users.noreply.github.com>
2023-03-30 19:46:10 +0500

    Add files via upload

---
+++ javascript3/index.js
@@ -0,0 +1,12 @@
+
+
+const userId = 123;
+let userName = "";
+let userAge = null;
+let isLogin = false;
+let object = {};
+let array = [1,2,3,4,5];
+let function1 = ()=>{};
+
+console.log(typeof function1 );
+
